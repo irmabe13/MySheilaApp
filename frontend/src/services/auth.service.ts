82c9e4c41a72ea97a0a7c5fe2804dc13f5049e76
irmabe <irma79200@gmail.com>
2025-10-01 16:07:52 +0200

    refactor AuthPage to use auth.service

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,30 +1,30 @@
-import {http} from "./http";
+import {api} from "../lib/api";
+import {setTokens} from "../lib/auth.ts";
 
 export type TokenPair = { accessToken: string; refreshToken: string };
 export type AuthRequest = { email: string; password: string };
 export type RegisterRequest = { firstname: string; lastname: string; email: string; password: string };
 
 export async function login(payload: AuthRequest): Promise<TokenPair> {
-    const {data} = await http.post<TokenPair>("/auth/login", payload);
-    localStorage.setItem("accessToken", data.accessToken);
-    localStorage.setItem("refreshToken", data.refreshToken);
+    const {data} = await api.post<TokenPair>("/auth/login", payload);
+    setTokens(data.accessToken, data.refreshToken);
     return data;
 }
 
 export async function register(payload: RegisterRequest): Promise<void> {
-    await http.post("/auth/register", payload);
+    await api.post("/auth/register", payload);
 }
 
 export async function refresh(): Promise<string> {
     const rt = localStorage.getItem("refreshToken");
     if (!rt) throw new Error("Pas de refresh token");
-    const {data} = await http.post<TokenPair>("/auth/refresh-token", {refreshToken: rt});
-    localStorage.setItem("accessToken", data.accessToken);
+    const {data} = await api.post<TokenPair>("/auth/refresh-token", {refreshToken: rt});
+    setTokens(data.accessToken, data.refreshToken);
     return data.accessToken;
 }
 
 export async function logout(): Promise<void> {
-    await http.post("/auth/logout");
+    await api.post("/auth/logout");
     localStorage.removeItem("accessToken");
     localStorage.removeItem("refreshToken");
 }
